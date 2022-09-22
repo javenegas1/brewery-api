62b774cb6191f07de763fe2eca79b8381c4fbd1c
Juan <venegas.a.juan37@gmail.com>
2022-09-22 02:04:27 -0400

    profile and comment troubleshooting

--- breweries-api-backend/controllers/main-controller.js
+++ breweries-api-backend/controllers/main-controller.js
@@ -11,28 +11,21 @@ const Comment = require("../models/Comment")
     //route favorites array to profile page
 
 //comments
-    //most likely will be separate schema 
-    //will have three keys (for now), page id, user id, and comment string
+    //difficulty with appending req.body from form on frontend to display as string comment on backend
     //page id should allow it to keep comments on page about brewery with fetch call
     //user id should allow matching users to realize CRUD operations on their comment
 
-//setup events?
-    //most likely separate schema that will take in 4 keys (for now)
-    //user, location(brewery id), date, and small paragraph
-    //individual brewery page can call from this DB to fetch matching id's and list 
-    //events that fall under that brewery 
-
 //----------------------------------------------------------->
 //profile page
-// router.get("/profile", requireToken, async (req, res) => {
-//     try{
-//         const thisUser = await User.findOne({username: req.user.username})
-//         console.log(thisUser)
-//         res.json(thisUser)
-//     } catch (error){
-//         res.status(400).json(error)
-//     }
-//   });
+router.get("/profile", requireToken, async (req, res) => {
+    try{
+        const thisUser = await User.findOne({username: req.user.username})
+        console.log(thisUser)
+        res.json(thisUser)
+    } catch (error){
+        res.status(400).json(error)
+    }
+  });
 //----------------------------------------------------------->
 
 //favorite
