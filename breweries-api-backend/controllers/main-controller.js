eaac8d9fab23f98966b9311eecd4f7b2e8fde7f7
Juan <venegas.a.juan37@gmail.com>
2022-09-22 12:10:47 -0400

    git backend commenting functionality

--- breweries-api-backend/controllers/main-controller.js
+++ breweries-api-backend/controllers/main-controller.js
@@ -7,20 +7,15 @@ const { findById, find, findOneAndUpdate } = require("../models/User");
 const User = require("../models/User");
 const Comment = require("../models/Comment")
 
-//favorites
-    //route favorites array to profile page
-
 //comments
     //difficulty with appending req.body from form on frontend to display as string comment on backend
     //page id should allow it to keep comments on page about brewery with fetch call
     //user id should allow matching users to realize CRUD operations on their comment
 
-//----------------------------------------------------------->
 //profile page
 router.get("/profile", requireToken, async (req, res) => {
     try{
         const thisUser = await User.findOne({username: req.user.username})
-        // console.log(thisUser)
         let jsonUser = JSON.stringify(thisUser)
         console.log(jsonUser)
         res.json(thisUser)
@@ -28,7 +23,6 @@ router.get("/profile", requireToken, async (req, res) => {
         res.status(400).json(error)
     }
   });
-//----------------------------------------------------------->
 
 //favorite
 router.post('/favorites', requireToken, async(req, res) => {
@@ -61,12 +55,12 @@ router.post('/favorites', requireToken, async(req, res) => {
 router.post('/comment', requireToken, async (req, res) => {
     try{
         const thisUser = await User.findOne({username: req.user.username})
+        console.log(req.body)
         const newComment = await Comment.create({
-            username: thisUser.username,
-            comment: req.body.comment,
-            brewery: req.body.brewery
+            username: req.body.comment.username,
+            brewery: req.body.comment.brewery,
+            comment: req.body.comment.comment
         })
-        console.log(thisUser)
         console.log(newComment)
         res.status(200)
     } catch(error) {
